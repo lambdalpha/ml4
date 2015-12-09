5eeb1490261977e58ccecba88c5bb49bc922bc14
lambdalpha <jjqqpp432@163.com>
2015-12-09 17:16:54 +0800

    plot gaussians

--- matlab/chapter1/ch1.m
+++ matlab/chapter1/ch1.m
@@ -6,4 +6,31 @@ pg2 = comp_gauss_dens_val(m, S, x2)
 
 
 % plot Gaussian
+S = {};
 N = 500;
+m = [0 0]';
+S{1} = [1 0 ; 0 1];
+S{2} = [0.2 0 ; 0 0.2];
+S{3} = [2 0; 0 2];
+S{4} = [0.2 0; 0 2];
+S{5} = [2 0; 0 .2];
+S{6} = [1 0.5; 0.5 1];
+S{7} = [0.3 0.5; 0.5 2];
+S{8} = [0.3 -0.5; -0.5 2];
+
+figure
+for i = 1:8
+   Sigma = S{i};
+   data = gen_gaussian(m, Sigma, N);
+   subplot(3, 3,i);
+   plot(data(:, 1), data(:, 2), '.');
+   axis equal
+   axis([-7 7 -7 7])
+   %figure(i), plot(data(:, 1), data(:, 2), '.');
+   %figure(i), axis equal
+   %figure(i), axis([-7 7 -7 7])
+   
+end
+
+
+
